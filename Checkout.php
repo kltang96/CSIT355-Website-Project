dff173ace40df8d53445cd4386d1f9bfac7dc552
kltang96 <kltang96@hotmail.com>
2018-12-03 01:15:16 -0500

    cart and checkout

---
+++ Checkout.php
@@ -0,0 +1,65 @@
+﻿<?php
+	session_start();
+	$customerID = $_SESSION["customerID"] = "user1";
+
+	//connect to database
+	$servername = "localhost";
+	$dbname = "drinkDatabase";
+	$username = "super"; 
+	$password = "";
+    try {
+    	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    	// set the PDO error mode to exception
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	//echo "Connected successfully";
+    }
+    catch(PDOException $e) {
+    	echo "Connection failed: " . $e->getMessage();
+    }
+
+	//define functions
+	function writeOrder() {
+		$filename = "orders/order_$orderID.txt";
+		@$fp = fopen($filename, 'w');
+
+
+	}
+	function insertOrderToDB() {
+		$conn->query("INSERT INTO `orders`(`customerID`, `cost`, `status`, `orderdate`, `fulfilldate`, `request`) 
+			VALUES (" + $GLOBALS['customerID'] + ",0,'pending],timestamp,null,'')");
+
+
+	}
+	 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="bootstrap-4.1.3-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="Orders.css">
+
+</head>
+<body>
+    <h1> Orders </h1>
+    <div class="container ">
+		<form action="Orders.php" method="post">
+
+
+
+
+
+		</form>
+        <?php
+			writeOrder();
+		?>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
