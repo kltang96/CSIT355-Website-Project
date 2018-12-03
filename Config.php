e2c0d678a9bc91692a27e1c089bd931e9818a874
Genocidereaper <44594756+Genocidereaper@users.noreply.github.com>
2018-12-03 01:15:38 -0500

    Create Config.php
    this is config page for my php

---
+++ Config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost:3036');
+   define('DB_USERNAME', 'root');
+   define('DB_PASSWORD', 'rootpassword');
+   define('DB_DATABASE', 'database');
+   $db = mysqli_connect('localhost:3306', 'garbuttz_garbutt', 'rex132660!', 'garbuttz_project');
+?>
